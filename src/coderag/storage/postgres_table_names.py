aafd450a738e84d64c4b6056db3f1067664da6cf
Gerardo Herrera <gherrerz@gmail.com>
2026-05-11 15:41:04 -0400

    cambio menor nombres canónicos de tablas PostgreSQL

--- src/coderag/storage/postgres_table_names.py
+++ src/coderag/storage/postgres_table_names.py
@@ -1,5 +1,5 @@
 """Nombres canónicos de tablas PostgreSQL usadas por storage."""
 
-POSTGRES_JOBS_TABLE = "tbl_repository_jobs"
-POSTGRES_REPOS_TABLE = "tbl_repository_repos"
-POSTGRES_LEXICAL_CORPUS_TABLE = "tbl_repository_lexical_corpus"
+POSTGRES_JOBS_TABLE = "Tbl_Repository_Jobs"
+POSTGRES_REPOS_TABLE = "Tbl_Repository_Repos"
+POSTGRES_LEXICAL_CORPUS_TABLE = "Tbl_Repository_LexicalCorpus"
